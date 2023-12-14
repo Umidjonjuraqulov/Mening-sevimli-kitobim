e43403d869719f2a9b0869fafa9061298d1d037c
Umidjon <147255061+Umidjonjuraqulov@users.noreply.github.com>
2023-12-14 12:42:49 +0500

    Add files via upload

---
+++ pages/Juraqulova_Dilafruz.py
@@ -0,0 +1,6 @@
+import streamlit as st 
+
+st.title("Sayt yaratuvchisi")
+st.header("Juraqulova Dilafruz 8'b'-sinf o'quvchisi")
+st.write("Juraqulova Dilafruzning 8-sinf II-chorak INFORMATIKA fanidan loyiha ishi")
+st.image("images/Capture.PNG")
